98901ed3b84da1f3e76da167726316d56f7fd18b
Caio Souza <caiobsouza@hotmail.com>
2018-06-12 11:18:40 -0300

    Implemented short id generation with CRC

--- src/models/guest.js
+++ src/models/guest.js
@@ -1,9 +1,35 @@
 const mongoose = require('mongoose');
+const crc = require('crc');
+
+const generate_id = () => {
+    return crc.crc32(new Date().toISOString()).toString(36).toUpperCase();
+};
 
 const GuestSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        default: generate_id
+    },
     name: {
         type: String,
         required: true
+    },
+    relationship: {
+        type: String,
+        enum: [
+            'FAMILIA',
+            'AMIGO',
+            'COLEGA',
+            'OUTRO'
+        ]
+    },
+    confirmed: {
+        type: Boolean,
+        default: false
+    },
+    shallConfirm: {
+        type: Boolean,
+        default: false
     }
 });
 
