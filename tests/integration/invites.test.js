e3cb33feb0b1cd93e5096bbb83765601eae27d90
Caio Souza <caiobsouza@hotmail.com>
2018-06-13 16:32:58 -0300

    Fixed test description

--- tests/integration/invites.test.js
+++ tests/integration/invites.test.js
@@ -69,7 +69,7 @@ describe('[Integration] Invites', () => {
             });
     });
 
-    it('should do something', done => {
+    it('should add a guest to an invite', done => {
         const GUEST = '5b216b3606618e1645a3611e';
 
         chai.request(server)
