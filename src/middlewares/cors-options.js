e4de8e24a846de9cada40d3d513015ff057e22fd
Caio Souza <caiobsouza@hotmail.com>
2018-08-06 21:59:00 -0300

    Working on CORS configuration

--- src/middlewares/cors-options.js
+++ src/middlewares/cors-options.js
@@ -1,7 +1,12 @@
-const options = {
-    origin: [ /\.gabiecaio\.site$/, /\.gabiecaio\.local$/],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    optionsSuccessStatus: 200
+const OPTIONS = {
+    origin: [
+        'https://gabiecaio.site',
+        'http://gabiecaio.site',
+        'http://gabiecaio.local',
+        /\.caiosouza\.me$/
+    ],
+    method: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    optionsSuccessStatus: 200 //Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-module.exports = options;
+module.exports = OPTIONS;
