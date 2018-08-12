43a3b7d29a5d73b1ba822b052b1926192d8c1288
Caio Souza <caiobsouza@hotmail.com>
2018-08-12 11:54:01 -0300

    Removing fucking cors custom options

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,9 +1,8 @@
 const { json, urlencoded } = require('body-parser');
-const corsOptions = require('./middlewares/cors-options');
 const cors = require('cors');
 
 module.exports = (app) => {
-    app.use(cors(corsOptions));
+    app.use(cors());
     app.use(json());
     app.use(urlencoded({ extended: false }));
 };
